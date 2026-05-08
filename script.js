e3119106862cf12906b297ba3b3ac0cdc6d165f0
raheemspence <heem1284@gmail.com>
2026-05-08 17:02:12 -0400

    Add input validation and add delete note functionality

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const classes = [
     {id: "spanish", name: "SPANISH 101", description: "class about Spanish"}
 ]
 
-const notes = [
+let notes = [
     {id: 1, classId: "chem", text: "Today we learned about organic compounds and how they interact with one another"},
     {id: 2, classId: "bio", text: "For this lecture, we discussed anatomy."},
     {id: 3, classId: "chem", text: "We learned about chelation today"},
@@ -56,11 +56,17 @@ if (classesContainer) {
 const cleanedHash = window.location.hash.substring(1);
 const propVal = String(cleanedHash);
 
-// Get class object
-const clsObj = classes.find(cls => cls.id === propVal);
+// Create div element for textarea and new note button
+const newNoteContainer = document.createElement('div');
 
-// Get all note objects for specific class
-const noteObjs = notes.filter(note => note.classId === propVal);
+// Create textarea element for note
+const noteTextarea = document.createElement('textarea');
+noteTextarea.cols = 65;
+noteTextarea.placeholder = "Take a note..."
+
+// Create button to create note
+const createBtn = document.createElement('button');
+createBtn.textContent = "Create"
 
 // Create H1 element
 const heading = document.createElement('h1');
@@ -72,23 +78,119 @@ const para = document.createElement('p');
 const notesContainer = document.createElement('div');
 
 
-// Create paragraph for each note
-for (const note of noteObjs) {
-    const noteContent = document.createElement('p');
-    noteContent.classList.add('note');
-    noteContent.textContent = note.text;
-    notesContainer.appendChild(noteContent);
-}
+renderNotes(propVal);
 
-
-// Fill elements with class info
-heading.textContent = clsObj.name;
-para.textContent = clsObj.description;
-
+// Add elements to their css classes
 heading.classList.add('class-heading');
 para.classList.add('class-para');
 notesContainer.classList.add('notesContainer');
+newNoteContainer.classList.add('new-note-container');
+createBtn.classList.add('create-btn');
+
+newNoteContainer.appendChild(noteTextarea);
+newNoteContainer.appendChild(createBtn);
 
 document.body.appendChild(heading);
 document.body.appendChild(para);
+document.body.appendChild(newNoteContainer);
 document.body.appendChild(notesContainer);
+
+
+// Create note
+// Event listener for click of create btn
+createBtn.addEventListener('click', event => {
+    const newNoteContent = noteTextarea.value;
+
+    // Validate input
+    const valid = validateInput(newNoteContent);
+
+    if (!valid) {
+        return;
+    }
+
+    // Create new note object
+    // Object ID
+    const objId = Date.now();
+
+    // Class ID
+    const clsId = propVal;
+
+    const newNoteObj = {id: objId, classId: clsId, text: newNoteContent};
+
+    // Push new note object to notes array
+    notes.push(newNoteObj);
+
+    // Clear input
+    noteTextarea.value = "";
+
+    // Render notes
+    renderNotes(propVal);
+})
+
+
+// Render function to remove duplicate logic for rendeing notes
+function renderNotes(classId) {
+
+    // Clear all notes
+    notesContainer.replaceChildren();
+
+    // Get class object
+    const clsObj = classes.find(cls => cls.id === classId);
+
+    // Get all note objects for specific class
+    const noteObjs = notes.filter(note => note.classId === classId);
+
+    // Create paragraph for each note
+    for (const note of noteObjs) {
+        const noteDiv = document.createElement('div');
+        const noteContent = document.createElement('p');
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = "Delete";
+
+        // Attach note id to delete button
+        deleteBtn.dataset.id = note.id;
+  
+        noteContent.textContent = note.text;
+        noteDiv.appendChild(noteContent);
+        noteDiv.appendChild(deleteBtn);
+
+
+        // Add to appropriate css class
+        deleteBtn.classList.add('delete-btn');
+        noteDiv.classList.add('note-div');
+        noteContent.classList.add('note');
+        notesContainer.appendChild(noteDiv);
+    }
+
+    // Fill elements with class info
+    heading.textContent = clsObj.name;
+    para.textContent = clsObj.description;
+}
+
+
+// Function to validate input when creating notes
+function validateInput(text) {
+    if (text.trim() === "") {
+        alert("Invalid Input. Try again.");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// Event delegation of Notes Container
+notesContainer.addEventListener('click', event => {
+
+    // Delete note
+    if (event.target.classList.contains('delete-btn')) {
+        // Get note id via delete button id
+        const noteId = event.target.dataset.id;
+
+        console.log(noteId);
+
+        // Remove the note object with specific note id
+        notes = notes.filter(note => note.id !== Number(noteId));
+
+        renderNotes(propVal);
+    }
+})
