e8e4ff0aea28abf20c4c18ef11d70e773c0ea09e
raheemspence <heem1284@gmail.com>
2026-07-05 16:35:50 -0400

    feat: build notes page layout with course icons

--- js/app.js
+++ js/app.js
@@ -1,33 +1,8 @@
-const toggleButton = document.getElementById('toggle-btn')
-const sidebar = document.getElementById('sidebar')
+const ul = document.getElementById('course-list')
 
+ul.addEventListener('click', (event) => {
+    const parentElement = event.target;
 
-function toggleSideBar() {
-    sidebar.classList.toggle('show')
-    // toggleButton.classList.toggle('rotate')
+    const clickedLi = parentElement.closest('li');
 
-    // closeAllSubMenus()
-}
-
-// function toggleSubMenu(button) {
-
-//     if(!button.nextElementSibling.classList.contains('show')) {
-//         closeAllSubMenus()
-//     }
-
-//     button.nextElementSibling.classList.toggle('show')
-//     button.classList.toggle('rotate')
-
-//     if(sidebar.classList.contains('close')) {
-//         sidebar.classList.toggle('close')
-//         toggleButton.classList.toggle('rotate')
-//     }
-// }
-
-
-// function closeAllSubMenus() {
-//     Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
-//         ul.classList.remove('show')
-//         ul.previousElementSibling.classList.remove('rotate')
-//     })
-// }
+});
